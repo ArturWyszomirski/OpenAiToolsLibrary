c89e712b8ce0a3c6152caeb05389ad02bf610661
Artur Wyszomirski <artur.d.w@gmail.com>
2024-06-28 20:50:55 +0200

    Add embedding model selection.

--- src/OpenAiToolsLibrary/Services/EmbeddingService.cs
+++ src/OpenAiToolsLibrary/Services/EmbeddingService.cs
@@ -4,11 +4,17 @@ public class EmbeddingService(IOpenAiClientService openAiClient) : IEmbeddingSer
 {
     readonly OpenAIClient? _client = openAiClient.GetClient();
 
-    public async Task<float[]> GenerateEmbeddingAsync(IList<string> texts)
+    public List<string> EmbeddingModels { get; private set; } = ["text-embedding-3-small", "text-embedding-3-large", "text-embedding-ada-002"];
+    public string? SelectedEmbeddingModel { get; set; } = "text-embedding-ada-002";
+
+    public async Task<float[]> GenerateEmbeddingAsync(IList<string> texts, string? embeddingModel = default)
     {
         if (_client == null) throw new NullReferenceException("Open AI client is null.");
 
-        var options = new EmbeddingsOptions("text-embedding-3-small", texts);//"text-embedding-ada-002"
+        if (string.IsNullOrWhiteSpace(embeddingModel))
+            embeddingModel = !string.IsNullOrWhiteSpace(SelectedEmbeddingModel) ? SelectedEmbeddingModel : EmbeddingModels.FirstOrDefault();
+
+        var options = new EmbeddingsOptions(embeddingModel, texts);
 
         var response = await _client.GetEmbeddingsAsync(options);
         var embeddings = response.Value.Data
--- src/OpenAiToolsLibrary/Services/IEmbeddingService.cs
+++ src/OpenAiToolsLibrary/Services/IEmbeddingService.cs
@@ -3,5 +3,8 @@ namespace OpenAiToolsLibrary.Services;
 
 public interface IEmbeddingService
 {
-    Task<float[]> GenerateEmbeddingAsync(IList<string> texts);
+    Task<float[]> GenerateEmbeddingAsync(IList<string> texts, string? embeddingModel = default);
+
+    public List<string> EmbeddingModels { get; }
+    public string? SelectedEmbeddingModel { get; set; }
 }
